d6ce1696a81ea97bc985ae7b4315e19313e909b3
Jeremy <jeremy.p.bizon@student158.org>
2021-10-07 11:46:31 -0500

    fixed import :))

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/layout/Header";
+import Login from "./components/Login";
 import Logout from "./components/layout/Logout";
 function App() {
 	return (
