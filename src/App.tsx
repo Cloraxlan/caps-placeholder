0a389727edde46b169bd1f9a7697f5f4f95bf05d
Ravenr_ <marcello.v.pezzella@gmail.com>
2021-10-28 11:38:15 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Layout from "./components/Layout/Layout";
 import "./App.css";
+import Calendar from "./components/Calendar/Calendar";
 // const bodyStyling: CSSProperties = {
 // 	backgroundColor: "black",
 // };
