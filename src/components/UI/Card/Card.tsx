b5fd2ee982aa228a85f508e43cd29624310e02bf
Ravenr_ <marcello.v.pezzella@gmail.com>
2021-12-06 11:44:47 -0600

    made className optional for Card

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,8 +1,9 @@
 import { ReactChild, ReactFragment, ReactPortal } from 'react';
 import './Card.css';
 
-const Card = (props: { className: string; children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }) => {
-    const classes = 'card ' + props.className;
+const Card = (props: { className?: string; children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }) => {
+    let classes = 'card';
+    if(props.className) classes += props.className;
 
     return (<div className={classes}>{props.children}</div>);
 }
