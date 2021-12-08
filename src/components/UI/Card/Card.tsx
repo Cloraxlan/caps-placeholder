5a13d9e40251d1064c9e1aeac3c74a943b58a83f
Ravenr_ <marcello.v.pezzella@gmail.com>
2021-12-08 11:58:05 -0600

    style updates + fixed Card component

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -3,7 +3,7 @@ import './Card.css';
 
 const Card = (props: { className?: string; children: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; }) => {
     let classes = 'card';
-    if(props.className) classes += props.className;
+    if(props.className) classes = props.className + ' card';
 
     return (<div className={classes}>{props.children}</div>);
 }
