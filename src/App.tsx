9b80e65654b7f2092c516310e453a937b8bf207a
Ravenr_ <marcello.v.pezzella@gmail.com>
2022-01-05 11:31:34 -0600

    added some comments

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,12 @@ import Calendar from "./components/Calendar/Calendar";
 function App() {
 	return (
 		<body>
+
+			{/* Centers content on each page */}
 			<div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+				{/* Renders page content with a toolbar */}
 				<Layout>
+					{/* links toolbar nav items to pages */}
 					<Switch>
 						<Route path="/LoginPage" component={LoginPage} />
 						<Route path="/Search" component={Search} />
