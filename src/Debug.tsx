b8e97501f2d6f8c19fa3cac2709b50889b3c2cd4
Konrad Rozpadek <sbluek546@gmail.com>
2022-01-16 18:50:38 -0600

    Identification of bulk and unitless

--- src/Debug.tsx
+++ src/Debug.tsx
@@ -2,18 +2,36 @@ import React from 'react'
 import BulkIngredient from './Interfaces-Classes/BulkIngredient'
 import Ingredient, {  } from './Interfaces-Classes/Ingredient'
 import { constructIngredientFromString } from './Interfaces-Classes/Recipe'
+import Unit from './Interfaces-Classes/Unit'
 
 interface Props {
     
 }
 
 const Debug = (props: Props) => {
-    
+    const LITER: Unit = {
+	fullName: "Liter",
+	abbreviations: ["L", "L."],
+	system: "METRIC",
+	measure: "VOLUME",
+	convertionFactor: 1,
+}
     let x: Ingredient =  constructIngredientFromString("5 cups of apples")
-    if(x.measure == "VOLUME"){
-        console.log(x.magnitude+ " " + (x as BulkIngredient).unit )
-        console.log((x as BulkIngredient).unit)
-    }
+    let y: Ingredient =  constructIngredientFromString("five pints of butter")
+        let z: Ingredient =  constructIngredientFromString("fifty-seven vannila bean pods")
+    let ingredients: Ingredient[] = [x,y,z];
+    ingredients.map((i)=>{
+        if(i.isBulk()){
+            let  c : BulkIngredient = (i as BulkIngredient);
+            c.convertUnits(LITER);
+            console.log(c.magnitude +" " + c.unit.fullName + " of "+ c.name)
+        }else{
+                        console.log(i.magnitude + " of "+ i.name)
+
+        }
+    })
+
+    
     return (
         <div>
             {x.magnitude}
--- src/Interfaces-Classes/BulkIngredient.ts
+++ src/Interfaces-Classes/BulkIngredient.ts
@@ -1,5 +1,6 @@
 import Ingredient from "./Ingredient";
 import Unit from "./Unit";
+import UnitSystem from "./UnitSystem";
 //NOTE ONLY VOLUME IS SUPPORTED RN
 export default class BulkIngredient extends Ingredient {
 	private _unit : Unit;
@@ -14,6 +15,13 @@ export default class BulkIngredient extends Ingredient {
 	}
 	public get unit(){
 		return this._unit
+	}
+	//Converts into diffrent units and changes the unit of the class
+	public convertUnits(unitToConvertInto: Unit){
+		let converter = new UnitSystem(this._unit);
+		this._unit = unitToConvertInto;
+		this._magnitude =  converter.convert(this._magnitude, unitToConvertInto);
+
 	}
 	
 }
