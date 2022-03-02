9e9626ab9b9ccc18c31e48d23343c24f7dd06157
Jeremy <jeremy.p.bizon@student158.org>
2022-03-02 11:57:47 -0600

    changes

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,15 +2,12 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 //import counterReducer from '../features/counter/counterSlice';
 import loginReducer from "../features/login/loginSlice";
 import calendarSlice from "../features/recipeSearch/calendarSlice";
-import prefrencesSlice from "../prefrencesSlice";
-import sessionSlice from "../sessionSlice";
+// import prefrencesSlice from "../prefrencesSlice";
 
 export const store = configureStore({
 	reducer: {
 		login: loginReducer,
 		calendar: calendarSlice,
-		prefrences: prefrencesSlice,
-		session: sessionSlice,
 	},
 });
 
